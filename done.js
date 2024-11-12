a1a104e4e8057ce7cbae1ea77307016c06c7f142
ArturSad753 <artur207962@student-schoolsofwestfield.org>
2024-11-12 15:05:30 +0000

    code

--- done.js
+++ done.js
@@ -1,1 +1,43 @@
 
+function main(){
+    let pChoice = 0;
+    let cChoice = 0;
+    while (pChoice == cChoice){
+        pChoice = userTurn();
+        cChoice = cpuTurn();
+        if (pChoice == cChoice) alert("Tie");
+    }
+    findWinner(pChoice,cChoice);
+}
+function userTurn(){
+    let pChoice = prompt("Choose between r, p, and c");
+    if (pChoice == "r" || pChoice == "p" || pChoice == "c") return (pChoice);
+    else{
+        alert("Bad input");
+        userTurn();
+    } 
+}
+function cpuTurn(){
+    let cpuChoice = 0;
+    let choice = Math.floor(Math.random()*2);
+    if (choice == 0) cpuChoice = "r";
+    else if (choice == 1) cpuChoice = "p";
+    else if (choice == 2) cpuChoice = "s";
+    return cpuChoice;
+}
+function findWinner(p,c){
+    let winner = "Something went wrong";
+    if (p == "r") {
+        if (c == s) winner = "Player";
+        else winner = "Computer";
+    }
+    else if (r == s){
+        if (c == p) winner = "Player";
+        else winner = "Computer";
+    }
+    else{
+        if (c == r) winner = "Player";
+        else winner = "Computer";
+    }
+    alert(winner+"Wins!");
+}
--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+/*main*/
+function main(){
+    let uChoice = 0;
+    let cChoice = 0;
+    while (uChoice == cChoice){
+        uChoice = userTurn();
+        cChoice = cpuTurn();
+        if (uChoice == cChoice){
+            alert("We both chose"+cChoice);
+        }
+    }
+    findWinner(uChoice,cChoice);
+}
+/*userTurn
+* @param: none
+* @return: string(r,p, or s)
+*/
+function userTurn(){
+    let choice = "";
+    while (choice != "r" && choice != "p" && choice != "s"){
+    choice = prompt("Enter r,p or s.");
+    if (choice == "r" || choice == "p" || choice == "s") return choice;
+    else userTurn;
+    }
+}
+/*cpuTurn
+* @param: none
+* @return: string (r,p, or s)
+*/
+function cpuTurn(){
+    let choice = Math.floor(Math.random()*2);
+    if (choice == 0) return "r";
+    else if (choice == 1) return "p";
+    else return "s";
 
+}
+/*findWinner
+* @param: u (srting), c (string)
+* @return: none 
+*/
+function findWinner(u,c){
+    let winner = "I";
+    if (u == "r") winner = "You";
+        
+
+    alert("You picked" +u+ "and I picked" +c+ ". " +winner+ " won that round.");
+    
+
+}
